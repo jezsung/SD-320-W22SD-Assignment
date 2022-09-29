badacf92aa7f7f9241e56e7c368cb27082373d81
Jaesung Hong <jezsung@gmail.com>
2022-09-29 16:54:18 -0500

    add equal function

--- SD-320-W22SD-Assignment/Form1.cs
+++ SD-320-W22SD-Assignment/Form1.cs
@@ -34,6 +34,7 @@ namespace SD_320_W22SD_Assignment
                 _storedOperation = "+";
 
                 label_StoredOperand.Text = $"{_storedOperand} {_storedOperation}";
+                label_NumberDisplay.Text = "";
 
                 return;
             }
@@ -73,6 +74,7 @@ namespace SD_320_W22SD_Assignment
                 _storedOperation = "-";
 
                 label_StoredOperand.Text = $"{_storedOperand} {_storedOperation}";
+                label_NumberDisplay.Text = "";
 
                 return;
             }
@@ -112,6 +114,7 @@ namespace SD_320_W22SD_Assignment
                 _storedOperation = "×";
 
                 label_StoredOperand.Text = $"{_storedOperand} {_storedOperation}";
+                label_NumberDisplay.Text = "";
 
                 return;
             }
@@ -151,6 +154,7 @@ namespace SD_320_W22SD_Assignment
                 _storedOperation = "÷";
 
                 label_StoredOperand.Text = $"{_storedOperand} {_storedOperation}";
+                label_NumberDisplay.Text = "";
 
                 return;
             }
@@ -185,11 +189,45 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Equal_Click(object sender, EventArgs e)
         {
+            if (_storedOperand == null || _storedOperation == null || _numberDisplay.Length == 0)
+            {
+                return;
+            }
 
+            double number = double.Parse(_numberDisplay);
+            double result = 0;
+
+            switch (_storedOperation)
+            {
+                case "+":
+                    result = (double)(_storedOperand + number);
+                    break;
+                case "-":
+                    result = (double)(_storedOperand - number);
+                    break;
+                case "×":
+                    result = (double)(_storedOperand * number);
+                    break;
+                case "÷":
+                    result = (double)(_storedOperand / number);
+                    break;
+            }
+
+            _numberDisplay = "";
+            _storedOperand = result;
+            _storedOperation = null;
+
+            label_StoredOperand.Text += $" {number} = ";
+            label_NumberDisplay.Text = result.ToString();
         }
 
         private void button_Number1_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             _numberDisplay = _numberDisplay == "0" ? "1" : _numberDisplay + "1";
 
             label_NumberDisplay.Text = _numberDisplay;
@@ -197,6 +235,11 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Number2_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             _numberDisplay = _numberDisplay == "0" ? "2" : _numberDisplay + "2";
 
             label_NumberDisplay.Text = _numberDisplay;
@@ -204,6 +247,11 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Number3_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             _numberDisplay = _numberDisplay == "0" ? "3" : _numberDisplay + "3";
 
             label_NumberDisplay.Text = _numberDisplay;
@@ -211,6 +259,11 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Number4_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             _numberDisplay = _numberDisplay == "0" ? "4" : _numberDisplay + "4";
 
             label_NumberDisplay.Text = _numberDisplay;
@@ -218,6 +271,11 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Number5_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             _numberDisplay = _numberDisplay == "0" ? "5" : _numberDisplay + "5";
 
             label_NumberDisplay.Text = _numberDisplay;
@@ -225,6 +283,11 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Number6_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             _numberDisplay = _numberDisplay == "0" ? "6" : _numberDisplay + "6";
 
             label_NumberDisplay.Text = _numberDisplay;
@@ -232,6 +295,11 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Number7_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             _numberDisplay = _numberDisplay == "0" ? "7" : _numberDisplay + "7";
 
             label_NumberDisplay.Text = _numberDisplay;
@@ -239,6 +307,11 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Number8_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             _numberDisplay = _numberDisplay == "0" ? "8" : _numberDisplay + "8";
 
             label_NumberDisplay.Text = _numberDisplay;
@@ -246,12 +319,22 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Number9_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             _numberDisplay = _numberDisplay == "0" ? "9" : _numberDisplay + "9";
 
             label_NumberDisplay.Text = _numberDisplay;
         }
         private void button_Number0_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             if (_numberDisplay != "0")
             {
                 _numberDisplay += "0";
@@ -262,6 +345,11 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Sign_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             if (_numberDisplay.Length == 0 || _numberDisplay == "0")
             {
                 return;
@@ -281,6 +369,11 @@ namespace SD_320_W22SD_Assignment
 
         private void button_Point_Click(object sender, EventArgs e)
         {
+            if (_storedOperation == null)
+            {
+                label_StoredOperand.Text = "";
+            }
+
             if (_numberDisplay.Length == 0)
             {
                 _numberDisplay = "0.";
